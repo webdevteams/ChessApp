3c0631515e47096ac78bda7cab22ceb470aae8e2
vancerm <vancerm@rose-hulman.edu>
2023-04-19 16:10:09 -0400

    Initial setup for pages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,8 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
 
 rhit.ClassName = class {
 	constructor() {
